362f8ac0afa9d72324db4f6e7f69d4ab51a4c961
mahesh1088 <53685570+mahesh1088@users.noreply.github.com>
2019-08-04 15:59:04 +0530

    Create Arun.java

---
+++ Arun.java
@@ -0,0 +1,11 @@
+package com.sanumala;
+public class Arun
+{   
+      public static void main(String[] args)
+      {       
+         System.out.println("Arun kumar");   
+      }
+      
+      
+      
+      }
